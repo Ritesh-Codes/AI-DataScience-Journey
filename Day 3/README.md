75f1c4623fe091bcd70fcee8cf198a5e727136e6
Ritesh Patil <riteshpatil244@gmail.com>
2025-07-21 18:55:03 +0530

    Day 3: Arithmetic, comparison, logical operators in Python

---
+++ Day 3/README.md
@@ -0,0 +1,11 @@
+# Day 3 - Operators in Python
+
+## Topics Covered
+- Arithmetic Operators: +, -, *, /, //, %, **
+- Comparison Operators: ==, !=, >, <, >=, <=
+- Logical Operators: and, or, not
+
+## Files
+- `arithmetic_ops.py`
+- `comparison_ops.py`
+- `logical_ops.py`
---
+++ Day 3/arithmetic_ops.py
@@ -0,0 +1,10 @@
+a = 10
+b = 3
+
+print("Addition: ", a + b)
+print("Subtraction: ", a - b)
+print("Multiplication: ", a * b)
+print("Division: ", a / b) #Float division
+print("Floor division: ", a // b) # Integer division
+print("Modulus: ", a % b) #Remainder
+print("Exponent: ", a ** b) # Power (a^b)
